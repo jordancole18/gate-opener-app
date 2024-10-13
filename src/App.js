f9c1b7b843a90df217886d519e94cbfd6ece881e
Jordan Cole <jordan@sevenhillstechnology.com>
2024-10-12 20:09:27 -0400

    Do not trigger gate if a request is in process

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
   const [loading, setLoading] = useState(false);
 
   const triggerGate = () => {
+    if (loading) {
+      return;
+    }
+
     setLoading(true);
     fetch('/api/open', {
       method: 'POST',
